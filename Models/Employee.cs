d4356c6dc59ad8fa331c8d3cc7d42f5c92a846b0
austzdee <167780868+austzdee@users.noreply.github.com>
2025-11-14 21:26:03 +0000

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagementApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
+
